803a5080fcbbec1f21b67019bde5f2772ef5b830
suyeshtiwari23 <suyesh.tiwari@outlook.com>
2019-10-26 14:08:48 +0530

    icon file updated

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -37,9 +37,19 @@ export default function Home () {
             </div>
             <div className="belowC">
                 <div className="aboutContent">
-                    <h2>About Us</h2>
+                <h2>“WAY OF LIFE”</h2>
                     <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                      <p>The foundation of this school was laid on 18 th June 2018.This school runs under the aegis of Silochana Shikshan Samiti.</p>
+                      <p>Having classes from nursery to class 9 th and it follows the CBSE curriculum &amp; syllabus.</p>
+                      <p>With an excellent infrastructure it promises to impart quality education
+to all the young minds. Having excellent faculty from across the state to provide individualized care and support to their students. To
+innovate the minds of newly admitted ,they plan and encourage the
+students to participate in their own learning process.</p>
+                      <p>They plan various activities for infants to boast up the confidence and
+gradually prepare them for the upcoming challenges of their life.
+Instead of learning, they also encourage and support the talented souls
+towards their respective interests.
+They believe in encouraging the students to find their way of life.</p>
                     </p>
                 </div>
                 <div className="noticeWrapper">
